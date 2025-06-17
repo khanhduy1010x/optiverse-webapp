409f7f45f837a277d3c182b137a27e9a7fbb3607
*I0BDDgOAjY <nguyenkhanhduy92000@gmail.com>
2025-06-17 23:00:29 +0700

    OPTV-27 fix: fix logic refresh token and slidebarCss

--- src/components/layout/Sidebar.component.tsx
+++ src/components/layout/Sidebar.component.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Icon from '../common/Icon/Icon.component';
 import { NAV_SECTIONS } from '../common/Navigation/navigation';
+import './Sidebar.css';
 
 interface SliderBarProps {
   activeSection: string;
@@ -9,25 +10,41 @@ interface SliderBarProps {
 
 const SliderBar: React.FC<SliderBarProps> = ({ activeSection, onNavClick }) => {
   return (
-    <div className="w-16 bg-black h-screen py-18 flex flex-col items-center justify-between rounded-tr-xl rounded-br-xl ">
-      <div className="flex flex-col items-center justify-between flex-1 w-full">
-        {NAV_SECTIONS.map((section) => (
-          <button
-            key={section.path}
-            onClick={() => onNavClick(section.path)}
-            className={`flex items-center cursor-pointer justify-center w-12 h-12 rounded-lg transition-all duration-300 ${activeSection === section.path
-              ? 'bg-gray-800'
-              : 'hover:bg-gray-700'
-              }`}
-          >
-            <Icon
-              name={section.icon || 'home'}
-              size={36}
-              className={`${activeSection === section.path ? 'text-white' : 'text-gray-300'
-                }`}
-            />
-          </button>
-        ))}
+    <div>
+      <div className="slidebar-container w-16  bg-black h-screen py-18 flex flex-col items-center justify-between  relative z-10">
+        <div className="slidebar-container flex flex-col items-center justify-between flex-1 w-full">
+          {NAV_SECTIONS.map((section) => (
+            <div key={section.path} className="relative group w-full flex justify-center my-2 slidebar-container ">
+              <button
+                onClick={() => onNavClick(section.path)}
+                className={`group flex items-center cursor-pointer justify-center w-12 h-12 rounded-lg transition-all duration-300 ${activeSection === section.path
+                  ? 'bg-white'
+                  : 'hover:bg-white '
+                  }`}
+              >
+                <Icon
+                  name={section.icon || 'home'}
+                  size={36}
+                  className={`group-hover:text-black ${activeSection === section.path ? 'text-black ' : 'text-white '
+                    }`}
+                />
+
+              </button>
+
+              <div className="text-label-container absolute top-0 right-0 h-full -z-10 pointer-events-none overflow-visible">
+                <div
+                  className="sidebar-label bg-black text-white py-1 pr-2 whitespace-nowrap rounded-br-lg rounded-tr-lg absolute top-1/2 -z-1"
+                  style={{
+                    boxShadow: '0 2px 5px rgba(0,0,0,0.2)',
+                    left: '0'
+                  }}
+                >
+                  <span className="-z-10 text-sm">{section.label}</span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
