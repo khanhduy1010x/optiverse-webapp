21b05cbf4018ecee3ecc3a9f9e2af0f917495fa4
caotai <votai12315@gmail.com>
2025-12-08 17:31:12 +0700

    OPTV-44 fix: refactor code version 3

--- src/services/flashcard.service.ts
+++ src/services/flashcard.service.ts
@@ -150,11 +150,6 @@ class FlashcardService {
 
   public async createFlashcard(item: CreateFlashcardRequest): Promise<any> {
     try {
-      console.log('Creating flashcard with data:', {
-        deck_id: item.deck_id,
-        front: item.front,
-        back: item.back,
-      });
       const response = await api.post<ApiResponse<any>>(
         `${this.flashcardPath}`,
         {
@@ -168,8 +163,7 @@ class FlashcardService {
       return data;
     } catch (error: any) {
       console.error('Lỗi khi fetch API:', error);
-      console.error('Error response:', error.response?.data);
-      throw Error(error.response?.data?.message || error.message);
+      throw Error(error.message);
     }
   }
 
